a1ad1af7c67f7b370f86910f97d786c51e653a89
NowrinIslamMim <nowrin02@gmail.com>
2022-09-03 06:16:38 +0600

    modal added

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-// Fetch category items
 
-// const loadCategory = async() => {
-//     const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
-//     const data = await response.json();
-//     return data;
-// }
 
 const loadCategory = () => {
     const url = 'https://openapi.programming-hero.com/api/news/categories';
@@ -13,58 +7,30 @@ const loadCategory = () => {
         .then(data => setCategory(data.data.news_category))
 }
 const setCategory = (newsdata) => {
-    // console.log(newsdata);
-    // const data = loadCategory();
-    // const category = data.data.news_category;
-    // console.log(category);
+
     const categoryContainer = document.getElementById('category-container');
     const unique = [];
-    for (const news of newsdata) {
-        // console.log(news.category_id);
+    newsdata.forEach(news => {
+
         if (unique.indexOf((news.category_name) === -1)) {
 
             unique.push(news.category_name);
-            // console.log(news.category_id);
+
             const div = document.createElement('div');
-            div.innerHTML = `<li class="pointer" onclick="loadNews('${news.category_id}')" class="text-decoration-none" >${news.category_name}</li>`;
+            div.style.cursor = 'pointer';
+            div.innerHTML = `<li  onclick="loadNews('${news.category_id}')" class="text-decoration-none" >${news.category_name}</li>`;
             categoryContainer.appendChild(div);
         }
 
-    }
+    })
 }
 
-// const setCategory = async() => {
-//     const data = await loadCategory();
-//     const category = data.data.news_category;
-//     // console.log(category);
-//     const categoryContainer = document.getElementById('category-container');
-//     const unique = [];
-//     for (const news of category) {
-//         // console.log(news.category_id);
-//         if (unique.indexOf((news.category_name) === -1)) {
 
-//             unique.push(news.category_name);
-//             // console.log(news.category_id);
-//             const li = document.createElement('li');
-//             li.innerHTML = `<a href="" onclick="loadNews('${news.category_id}')" class="text-decoration-none" >${news.category_name}</a>`;
-//             categoryContainer.appendChild(li);
-//         }
-
-//     }
-// }
-// const loadNews = async (category_id) => {
-//     // console.log('clicked');
-//     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-//     console.log(url);
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     return data;
-// }
 
 const loadNews = category_id => {
-    console.log(category_id);
+    // console.log(category_id);
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displayNews(data.data))
@@ -72,14 +38,12 @@ const loadNews = category_id => {
 }
 
 const displayNews = (data) => {
-    // console.log(data.data);
-    // const data = loadNews();
-    // const newsData = data.data;
+
 
     const cardDiv = document.getElementById('card-container');
     cardDiv.innerText = "";
-    for (const news of data) {
-        // console.log(news);
+    data.forEach(news => {
+
         const div = document.createElement('div');
         div.innerHTML = `
     <div class="col">
@@ -91,7 +55,7 @@ const displayNews = (data) => {
 
                 </div>
                 <div class="card-footer">
-                <p class="card-text"> <img class="img-fluid custom-img" src="${news.author.img}"><small class="text-muted ms-2">${news.author.name ? news.author.name : "No Author Found"} <i class="fa-solid fa-eye ms-4"> ${news.total_view ? news.total_view : "No views"}</i><a class="ms-4" href="">Details<i class="fa-solid fa-arrow-right-long "></i></a></small></p>
+                <p class="card-text"> <img class="img-fluid custom-img" src="${news.author.img}"><small class="text-muted ms-2">${news.author.name ? news.author.name : "No Author Found"} <i class="fa-solid fa-eye ms-4"> ${news.total_view ? news.total_view : "No views"}</i><a class="ms-4" href="" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="loadDetail('${news._id}')">Details<i class="fa-solid fa-arrow-right-long "></i></a></small></p>
 
 
                 </div>
@@ -99,11 +63,32 @@ const displayNews = (data) => {
     </div>
 `;
         cardDiv.appendChild(div);
-    }
+    })
+}
+
+const loadDetail = newsId => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetails(data.data))
+}
+
+const showDetails = (data) => {
+
+    data.forEach(detail => {
+        const modalTitle = document.getElementById('staticBackdropLabel');
+        modalTitle.innerText = `${detail.title}`;
+
+        const modalBody = document.getElementById('modal-body');
+
+        modalBody.innerHTML = `<img class="img-fluid w-100 mb-4" src="${detail.thumbnail_url}">
+                 <p>${detail.details}</p>`;
+
+    })
+
 }
 
 
-// displayNews();
-// setCategory();
 loadCategory();
 
